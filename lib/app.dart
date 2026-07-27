c74532287cc0041849288d47ea71978c4e99431b
adtoba <adetoba53@gmail.com>
2026-07-27 14:58:38 +0100

    chore: app theme setup

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:grox/core/router/app_router.dart';
+import 'package:grox/core/theme/app_theme.dart';
 
 class GroxApp extends ConsumerWidget {
   const GroxApp({super.key});
@@ -12,6 +13,8 @@ class GroxApp extends ConsumerWidget {
     return MaterialApp.router(
       onGenerateTitle: (context) => 'Grox',
       routerConfig: router,  
+      theme: AppTheme.light,
+      themeMode: ThemeMode.light,
     );
   }
 }
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:grox/core/theme/app_colors.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData get light => _buildTheme(
+    brightness: Brightness.light, 
+    background: AppColors.white, 
+    onSurface: AppColors.grey10, 
+    secondary: AppColors.grey2
+  );
+
+
+  static ThemeData _buildTheme({
+    required Brightness brightness,
+    required Color background,
+    required Color onSurface,
+    required Color secondary,
+  }) {
+    final colorScheme = ColorScheme(
+      brightness: brightness,
+      onPrimary: AppColors.primary,
+      primary: AppColors.primary,
+      secondary: secondary,
+      onSecondary: secondary,
+      surface: background,
+      onSurface: onSurface,
+      error: AppColors.error,
+      onError: AppColors.error,
+    );
+
+    return ThemeData(
+      colorScheme: colorScheme,
+      scaffoldBackgroundColor: background,
+      useMaterial3: true,
+      brightness: brightness,
+      fontFamily: "Geist",
+      appBarTheme: AppBarTheme(
+        backgroundColor: background,
+
+      )
+    );
+  }
+}
